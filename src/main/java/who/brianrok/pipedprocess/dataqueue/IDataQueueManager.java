f52584f1e793ffce805998f37f0ebb646a336e2c
Zhengyu Hu <zhengyuh@amazon.com>
2017-04-06 23:37:50 +0800

    Add interface for data queue manager

---
+++ src/main/java/who/brianrok/pipedprocess/dataqueue/IDataQueueManager.java
@@ -0,0 +1,48 @@
+package who.brianrok.pipedprocess.dataqueue;
+
+import who.brianrok.pipedprocess.exception.DataQueueException;
+
+import java.util.List;
+
+/**
+ * Interface of data queue manager
+ */
+public interface IDataQueueManager {
+
+    int DEFAULT_CAPACITY = 1000;
+
+    /**
+     * Register a new queue with given name
+     * @param queueName Name of queue
+     * @throws DataQueueException When the queue is already exists
+     */
+    void registerDataQueue(String queueName) throws DataQueueException;
+
+    /**
+     * Register a new queue with given name and fixed capacity
+     * @param queueName Name of queue
+     * @param capacity Capacity of the queue
+     * @throws DataQueueException When the queue is already exists
+     */
+    void registerDataQueue(String queueName, int capacity) throws DataQueueException;
+
+    /**
+     * Get a queue with given name
+     * @param queueName Name of queue
+     * @throws DataQueueException When the queue does not exists
+     */
+    void getDataQueue(String queueName) throws DataQueueException;
+
+    /**
+     * Remove a finished queue
+     * @param queueName Name of queue
+     * @throws DataQueueException When the queue does not exists or is not finished
+     */
+    void removeDataQueue(String queueName) throws DataQueueException;
+
+    /**
+     * Get all names of registered queues
+     * @return All registered queue names
+     */
+    List<String> getAllQueueNames();
+}
---
+++ src/main/java/who/brianrok/pipedprocess/exception/DataQueueException.java
@@ -0,0 +1,7 @@
+package who.brianrok.pipedprocess.exception;
+
+/**
+ * Exception of Data Queues
+ */
+public class DataQueueException extends RuntimeException {
+}
